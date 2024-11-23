d43887a38d3eae5b9f3bd54f6e4a30f2a733b2db
superkran9 <superkran7@gmail.com>
2024-11-23 07:07:45 +0300

    lab3

--- neuralnet/neuralnet/urls.py
+++ neuralnet/neuralnet/urls.py
@@ -27,7 +27,10 @@ urlpatterns = [
 
 from django.contrib import admin
 from django.urls import include, path
+from .views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bert_classifier.urls')),
+    path('', index, name='index'),
+    path('bert_classifier/', include('bert_classifier.urls')),
+    path('dialog_bot/', include('dialog_bot.urls'))
 ]
