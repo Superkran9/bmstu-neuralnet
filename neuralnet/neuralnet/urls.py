acaa104758c3e3b2387872d712bb417e0dd53482
superkran9 <superkran7@gmail.com>
2024-11-23 05:18:04 +0300

    lab2

--- neuralnet/neuralnet/urls.py
+++ neuralnet/neuralnet/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+'''
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('bert_classifier/', include('bert_classifier.urls')),  # Маршруты приложения bert_classifier
+    path('', include('bert_classifier.urls')),  # Главная страница перенаправляется на bert_classifier
+]
+'''
+
 from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bert_classifier/', include('bert_classifier.urls')),
+    path('', include('bert_classifier.urls')),
 ]
